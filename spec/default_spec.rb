3cc1f0d05f05b0b8e88c7959aac893918afff5f4
Anuj Biyani <abiyani@lytro.com>
2013-01-25 17:39:27 -0800

    update spec_helper and default_spec with latest from chef_cookbook_template repo

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe 'teaching_chef_cookbooks::default' do
-  let(:chef_run) { @runner.converge 'teaching_chef_cookbooks::default' }
+  let(:chef_run) { runner.converge 'teaching_chef_cookbooks::default' }
 
   it "does something" do
     pending "I pity the fool who doesn't write specs!"
